609d6588023677578805d683c77e82e4beba3d11
DeepthiSDET169 <samudramdeepthi@gmail.com>
2024-07-31 17:02:55 -0500

    Add files via upload

---
+++ Day1.java
@@ -0,0 +1,54 @@
+
+
+
+
+public class Day1 {
+
+	public static void main(String[] args) {
+		int Basic_pay = 0;
+		String Null = null;
+		Day1 emp = new Day1();  	
+        emp.Employee_level(1);
+        emp.Basic_pay(15000);
+        emp. BOA(17000);
+        emp. Bonus(400);
+    	System.out.println(emp);
+
+    	if(Basic_pay>=0)
+		{
+			System.out.println("Existing Tax Rate is"+Basic_pay);
+		} else 
+		{
+            System.out.println("Existing Tax Rate is"+Null);
+            }
+		}
+
+	public void Bonus(int i) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void BOA(int i) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void Basic_pay(int i) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void Employee_level(int i) {
+		// TODO Auto-generated method stub
+		
+	}}
+	
+
+public class Day1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
